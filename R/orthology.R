4d9de40b432125a3088903dcedc1507a5ec471e3
Gregory Gedman <ggedman@rockefeller.edu>
2019-04-22 16:32:03 -0400

    Updated NAMESPACE for proper loading. Updated output from getOrthos function

--- R/orthology.R
+++ R/orthology.R
@@ -1,32 +1,50 @@
-#' get Orthologs 
+
+#' getOrthologs 
 #' 
 #' Get all one-to-one orthologs between a given species and a desired species 
 #' 
+#' @import biomaRt
 #' @param input_org Species you are starting from. This should be in the latin name format. i.e. Human, Homo Sapien should be "hsapiens"
-#' @param comp_org Species you want to know the orthologs in. Same format as input_org.  
+#' @param output_org Species you want to know the orthologs in. Same format as input_org.  
+#' @param one2one Specifies if one-2-one orthology is perferred. If set to TRUE, will only return orthology for one-2-one orthologs
 #' @return data frame with all one-to-one orthologs between specified species 
-#' 
-getOrthos <- function(input_org,comp_org) {
+#' @export
+getOrthos <- function(input_org,output_org,one2one) {
   tmp_input <- paste(input_org,"_gene_ensembl",sep = "")
   tmp_mart <- useMart("ensembl", dataset = tmp_input) 
+  tmp_attr <- listAttributes(tmp_mart)
   tmp_genes <- getBM(attributes = c("ensembl_gene_id", "gene_biotype"), mart = tmp_mart)
   tmp_genes <- tmp_genes[grep("protein_coding",tmp_genes$gene_biotype),]
-  tmp_orthos <- getBM(attributes = c("ensembl_gene_id","external_gene_name",paste(comp_org,"_homolog_ensembl_gene",sep=""),paste(comp_org,"_homolog_associated_gene_name",sep = ""),paste(comp_org,"_homolog_orthology_type",sep = "")), filters = "ensembl_gene_id",values = tmp_genes$ensembl_gene_id, mart = tmp_mart)
-  orthos_one2one <- tmp_orthos[grep("ortholog_one2one",tmp_orthos[,5]),]
+  attr_names <- tmp_attr[grep(output_org,tmp_attr$name),"name"]
+  tmp_orthos <- getBM(attributes = c("ensembl_gene_id","external_gene_name",attr_names), filters = "ensembl_gene_id",values = tmp_genes$ensembl_gene_id, mart = tmp_mart)
+  
+  if(one2one == TRUE){
+  tmp_orthos <- tmp_orthos[grep("ortholog_one2one",tmp_orthos[,paste(output_org,"_homolog_orthology_type",sep = "")]),]
+  }
+
+  if(one2one == FALSE){
+    tmp_orthos <- tmp_orthos
+  }
+  
+  tmp_orthos
+
 }
 
 #' get Gene Orthologs
 #' 
 #' Get the one-to-one ortholog for a specific gene between a given species and a desired species 
 #' 
+#' @import biomaRt
 #' @param input_org Species you are starting from. This should be in the latin name format. i.e. Human, Homo Sapien should be "hsapiens"
-#' @param comp_org Species you want to know the orthologs in. Same format as input_org.  
+#' @param output_org Species you want to know the orthologs in. Same format as input_org.  
 #' @param gene Ensembl ID of gene in the starting species. 
 #' @return data frame with all one-to-one orthologs between specified species 
-#' 
-getGeneOrthos <- function(input_org,comp_org,gene) {
+#' @export
+getGeneOrthos <- function(input_org,output_org,gene) {
   tmp_input <- paste(input_org,"_gene_ensembl",sep = "")
   tmp_mart <- useMart("ensembl", dataset = tmp_input) 
-  tmp_orthos <- getBM(attributes = c("ensembl_gene_id","external_gene_name",paste(comp_org,"_homolog_ensembl_gene",sep=""),paste(comp_org,"_homolog_associated_gene_name",sep = ""),paste(comp_org,"_homolog_orthology_type",sep = "")), filters = "ensembl_gene_id",values = gene, mart = tmp_mart)
+  tmp_attr <- listAttributes(tmp_mart)
+  attr_names <- tmp_attr[grep(output_org,tmp_attr$name),"name"]
+  tmp_orthos <- getBM(attributes = c("ensembl_gene_id","external_gene_name",attr_names), filters = "ensembl_gene_id",values = gene, mart = tmp_mart)
 }
 
